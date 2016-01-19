4f8156d4199a3c930409c272f1026bd430d1028e
Wanilton Figueiredo De M Filho <wanilton.filho@castmeta.local>
2016-01-19 09:00:30 -0200

    Initial commit

---
+++ app/src/main/java/com/example/waniltonfilho/personaltasks/controller/activities/MainLoginActivity.java
@@ -0,0 +1,7 @@
+package com.example.waniltonfilho.personaltasks.controller.activities;
+
+/**
+ * Created by wanilton.filho on 19/01/2016.
+ */
+public class MainLoginActivity {
+}
---
+++ app/src/main/java/com/example/waniltonfilho/personaltasks/model/a.java
@@ -0,0 +1,7 @@
+package com.example.waniltonfilho.personaltasks.model;
+
+/**
+ * Created by wanilton.filho on 18/01/2016.
+ */
+public class a {
+}
